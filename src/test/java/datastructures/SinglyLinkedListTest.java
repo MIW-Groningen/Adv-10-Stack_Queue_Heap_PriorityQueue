5a122ccc1f77329c9a74560394d3dcbcf8a8ef61
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-12-06 14:36:27 +0100

    Update to the test suite

--- src/test/java/datastructures/SinglyLinkedListTest.java
+++ src/test/java/datastructures/SinglyLinkedListTest.java
@@ -41,8 +41,38 @@ class SinglyLinkedListTest {
     }
 
     @Test
-    @DisplayName("4 - remove(index)")
+    @DisplayName("4a - remove(index) for a middle element")
     void remove() {
         ListTest.removeElement(intList);
+    }
+
+    @Test
+    @DisplayName("4b - remove(index) for the first element")
+    void removeFirst() {
+        ListTest.removeFirstElement(intList);
+    }
+
+    @Test
+    @DisplayName("4c - remove(index) for the last element")
+    void removeLast() {
+        ListTest.removeLastElement(intList);
+    }
+
+    @Test
+    @DisplayName("4d - remove(index) for the last element")
+    void removeFirstOfTwo() {
+        ListTest.removeFirstOfTwoElement(intList);
+    }
+
+    @Test
+    @DisplayName("4e - remove(index) for the last element")
+    void removeLastOfTwo() {
+        ListTest.removeLastOfTwoElement(intList);
+    }
+
+    @Test
+    @DisplayName("4f - remove(index) for the last element")
+    void removeOnlyElement() {
+        ListTest.removeOnlyElement(intList);
     }
 }
