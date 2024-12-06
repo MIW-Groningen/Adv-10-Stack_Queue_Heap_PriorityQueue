9e06a5b180dddad9efefcf8102ec41048f782af3
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-12-06 14:36:27 +0100

    Update to the test suite

--- src/test/java/datastructures/ListTest.java
+++ src/test/java/datastructures/ListTest.java
@@ -85,4 +85,82 @@ public class ListTest {
         );
     }
 
+    public static <L extends List<Integer>> void removeFirstElement(L intList) {
+        intList.add(0);
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+
+        int removed = intList.remove(0);
+
+        assertAll(
+                () -> assertEquals(4, intList.size(), "The list should have size 4"),
+                () -> assertEquals(0, removed, "The removed element should be 0"),
+                () -> assertEquals(1, intList.get(0), "The element at index 0 should be 1"),
+                () -> assertEquals(2, intList.get(1), "The element at index 1 should be 2"),
+                () -> assertEquals(3, intList.get(2), "The element at index 2 should be 3"),
+                () -> assertEquals(4, intList.get(3), "The element at index 3 should be 4"),
+                () -> assertEquals("[1, 2, 3, 4]", intList.toString())
+        );
+    }
+
+    public static <L extends List<Integer>> void removeLastElement(L intList) {
+        intList.add(0);
+        intList.add(1);
+        intList.add(2);
+        intList.add(3);
+        intList.add(4);
+
+        int removed = intList.remove(4);
+
+        assertAll(
+                () -> assertEquals(4, intList.size(), "The list should have size 4"),
+                () -> assertEquals(4, removed, "The removed element should be 4"),
+                () -> assertEquals(0, intList.get(0), "The element at index 0 should be 0"),
+                () -> assertEquals(1, intList.get(1), "The element at index 1 should be 1"),
+                () -> assertEquals(2, intList.get(2), "The element at index 2 should be 2"),
+                () -> assertEquals(3, intList.get(3), "The element at index 3 should be 3"),
+                () -> assertEquals("[0, 1, 2, 3]", intList.toString())
+        );
+    }
+
+    public static <L extends List<Integer>> void removeFirstOfTwoElement(L intList) {
+        intList.add(0);
+        intList.add(1);
+
+        int removed = intList.remove(0);
+
+        assertAll(
+                () -> assertEquals(1, intList.size(), "The list should have size 1"),
+                () -> assertEquals(0, removed, "The removed element should be 0"),
+                () -> assertEquals("[1]", intList.toString())
+        );
+    }
+
+    public static <L extends List<Integer>> void removeLastOfTwoElement(L intList) {
+        intList.add(0);
+        intList.add(1);
+
+        int removed = intList.remove(1);
+
+        assertAll(
+                () -> assertEquals(1, intList.size(), "The list should have size 1"),
+                () -> assertEquals(1, removed, "The removed element should be 1"),
+                () -> assertEquals("[0]", intList.toString())
+        );
+    }
+
+    public static <L extends List<Integer>> void removeOnlyElement(L intList) {
+        intList.add(0);
+
+        int removed = intList.remove(0);
+
+        assertAll(
+                () -> assertEquals(0, intList.size(), "The list should have size 1"),
+                () -> assertEquals(0, removed, "The removed element should be 1"),
+                () -> assertEquals("[]", intList.toString())
+        );
+    }
+
 }
