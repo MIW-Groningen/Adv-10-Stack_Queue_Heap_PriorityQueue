f9f39a0ddfef407bdca1d69374f16be9c547e815
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2024-12-06 14:36:10 +0100

    Answer for the Double Ended Doubly Linked List exercise

---
+++ src/main/java/datastructures/DoubleEndedDoublyLinkedList.java
@@ -0,0 +1,146 @@
+package datastructures;
+
+/**
+ * @author Vincent Velthuizen
+ * Purpose for the class
+ */
+public class DoubleEndedDoublyLinkedList<E> extends AbstractList<E> {
+    private Node<E> head;
+    private Node<E> tail;
+
+    private int size;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public E get(int index) {
+        return findNodeAt(index).getElement();
+    }
+
+    @Override
+    public void add(E element) {
+        Node<E> newNode = new Node<>(element);
+
+        if (size == 0) {
+            head = newNode;
+        } else {
+            newNode.setPrevious(tail);
+            tail.setNext(newNode);
+        }
+
+        tail = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        checkBounds(index, true);
+        Node<E> newNode = new Node<>(element);
+
+        if (index == 0) {
+            newNode.setNext(head);
+            if (head != null) {
+                head.setPrevious(newNode);
+            }
+            head = newNode;
+        }
+
+        if (index == size) {
+            if (tail != null) {
+                tail.setNext(newNode);
+            }
+            newNode.setPrevious(tail);
+            tail = newNode;
+        }
+
+        if (index > 1 && index < size) {
+            Node<E> current = findNodeAt(index);
+
+            current.getPrevious().setNext(newNode);
+            newNode.setPrevious(current.getPrevious());
+            newNode.setNext(current);
+            current.setPrevious(newNode);
+        }
+
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        Node<E> toBeRemoved = findNodeAt(index);
+
+        if (index == 0) {
+            head = head.getNext();
+            if (head != null) {
+                head.setPrevious(null);
+            }
+        }
+
+        if (index == size - 1) {
+            tail = tail.getPrevious();
+            if (tail != null) {
+                tail.setNext(null);
+            }
+        }
+
+        if (index > 1 && index < size - 1) {
+            toBeRemoved.getPrevious().setNext(toBeRemoved.getNext());
+            toBeRemoved.getNext().setPrevious(toBeRemoved.getPrevious());
+        }
+
+        size--;
+        return toBeRemoved.getElement();
+    }
+
+    private Node<E> findNodeAt(int index) {
+        checkBounds(index);
+
+        Node<E> current;
+        if (index < size / 2) {
+            current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.previous;
+            }
+        }
+
+        return current;
+    }
+
+    private static class Node<E> {
+        private final E element;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E element) {
+            this.element = element;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+
+        public Node<E> getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node<E> previous) {
+            this.previous = previous;
+        }
+    }
+}
---
+++ src/test/java/datastructures/DoubleEndedDoublyLinkedListTest.java
@@ -0,0 +1,78 @@
+package datastructures;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Vincent Velthuizen
+ * Test the SinglyLinkedList for all basic List operations
+ */
+class DoubleEndedDoublyLinkedListTest {
+    private DoubleEndedDoublyLinkedList<Integer> intList;
+
+    @BeforeEach
+    public void initialize() {
+        intList = new DoubleEndedDoublyLinkedList<>();
+    }
+
+    @Test
+    @DisplayName("1a & 2a - add() and get() (and toString which was given)")
+    void addOne() {
+        ListTest.addOne(intList);
+    }
+
+    @Test
+    @DisplayName("1b & 2b - add() and get() for a longer list")
+    void addMultiple() {
+        ListTest.addMultiple(intList);
+    }
+
+    @Test
+    @DisplayName("3a - add(index, element) for a minimal example")
+    void addAtIndex() {
+        ListTest.addAtIndex(intList);
+    }
+
+    @Test
+    @DisplayName("3b - add(index, element) for a larger example")
+    void addMultipleAtIndex() {
+        ListTest.addMultipleAtIndex(intList);
+    }
+
+    @Test
+    @DisplayName("4a - remove(index) for a middle element")
+    void remove() {
+        ListTest.removeElement(intList);
+    }
+
+    @Test
+    @DisplayName("4b - remove(index) for the first element")
+    void removeFirst() {
+        ListTest.removeFirstElement(intList);
+    }
+
+    @Test
+    @DisplayName("4c - remove(index) for the last element")
+    void removeLast() {
+        ListTest.removeLastElement(intList);
+    }
+
+    @Test
+    @DisplayName("4d - remove(index) for the last element")
+    void removeFirstOfTwo() {
+        ListTest.removeFirstOfTwoElement(intList);
+    }
+
+    @Test
+    @DisplayName("4e - remove(index) for the last element")
+    void removeLastOfTwo() {
+        ListTest.removeLastOfTwoElement(intList);
+    }
+
+    @Test
+    @DisplayName("4f - remove(index) for the last element")
+    void removeOnlyElement() {
+        ListTest.removeOnlyElement(intList);
+    }
+}
